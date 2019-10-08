8d18e4d0d16cc2fb1057dd100c1c4205c38b4bdd
Chris Kerbo <chris.kerbo@outlook.com>
2019-10-08 07:14:42 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,7 +5,7 @@ html = open("https://flatironschool.com/")
 
 doc = Nokogiri::HTML(html)
 
-courses = doc.css(".title-oE5vT4").text
+courses = doc.css(".title-oE5vT4")
 courses.each do |course|
   puts course.text.strip
 end
